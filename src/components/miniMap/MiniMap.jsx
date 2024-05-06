5fea71cff0f34848406eaff486c27cd233500cb9
Ricardo Costa <ricardo.costa@twisted-rope.com>
2024-05-06 09:46:16 +0200

    Refactors minimap

--- src/components/miniMap/MiniMap.jsx
+++ src/components/miniMap/MiniMap.jsx
@@ -1,42 +1,132 @@
 "use client";
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useImperativeHandle } from 'react';
 import "./miniMap.css";
 
-export default function MiniMap({ scene, thumbnailURL, sceneScroll, background }) {
+export default function MiniMap({ sceneRef, backgroundRef, backgroundURL, miniMapRef }) {
 
+  // MINI MAP
+
+  let targetPosition;
+  let isDownMinimap = false;
+  let currentPosition;
+  let controlOffset;
+  let minX;
+  let maxX;
+  let padding = 0;
+  let sceneWidth;
+  let startX;
+  
   const miniMap = useRef();
   const control = useRef();
   const [controlWidth, setControlWidth] = useState();
-  const [targetPosition, setTargetPosition] = useState(0);
 
-  let isDown = false;
-  let sceneWidth = background.current?.offsetWidth || 0;
+  // Control handlers
 
-  window.addEventListener("resize", updateControlWidth);
-  useEffect(updateControlWidth)
+  useEffect(() => {
 
-  useEffect(()=>{
-    updateControlPosition(sceneScroll)
-  }, [sceneScroll])
+    sceneWidth = backgroundRef.current?.offsetWidth || 0;
+    controlOffset = control.current.offsetWidth / 2;
+    minX = controlOffset;
+    maxX = miniMap.current.offsetWidth - controlOffset;
 
-  function updateControlWidth(){
+    control.current.style.left = 0;
+
+    miniMap.current.addEventListener('mousedown', mouseUp);
+    miniMap.current.addEventListener('touchstart', mouseUp);
+
+    miniMap.current.addEventListener('mouseup', mouseDown);
+    miniMap.current.addEventListener('touchend', mouseDown);
+
+    miniMap.current.addEventListener('mouseup', mouseDown);
+    miniMap.current.addEventListener('touchend', mouseDown);
+
+    miniMap.current.addEventListener('mousemove', mouseMove);
+    miniMap.current.addEventListener('touchmove', mouseMove);
+
+    miniMap.current.addEventListener('mouseenter', () => {
+      isDownMinimap = false;
+      miniMap.current.classList.remove('scrolling');
+    });
+
+    // These two are flipped for some reason. WTF.
+    function mouseUp(e) {
+      isDownMinimap = true;
+      control.current.classList.add('scrolling');
+      startX = e.pageX - miniMap.current.offsetLeft;
+      currentPosition = control.current.style.left;
+    }
+
+    function mouseDown() {
+      isDownMinimap = false;
+      control.current.classList.remove('scrolling');
+    }
+
+    function mouseMove(e) {
+
+      // If mouse is up, do nothing
+      if (!isDownMinimap) return;
+      e.preventDefault();
+
+      // Gets mouse X relative to the minimap
+      if (e.pageX !== undefined) {
+        targetPosition = e.pageX - miniMap.current.offsetLeft;
+      } else {
+        // Touch screen support
+        targetPosition = e.changedTouches[0].pageX - miniMap.current.offsetLeft;
+      }
+
+      // Clamps control.current position
+      if (targetPosition < minX + padding) { targetPosition = minX; }
+      else if (targetPosition > maxX - padding) { targetPosition = maxX - padding; }
+
+      // Offsets for the frame size
+      targetPosition = targetPosition - controlOffset;
+
+      // Updates the position of the minimap's control.current
+      control.current.style.left = targetPosition + "px";
+
+      // Normalizes minimap position
+      let normalizedPosition = targetPosition / (miniMap.current.offsetWidth - controlOffset / 2);
+
+      // Scrolls scene
+      sceneRef.current.scrollLeft = normalizedPosition * sceneWidth;
+
+    }
+
+  });
+
+  useEffect(() => {
+    // Updates minimap control control.current width 
+    updateControlWidth();
+    window.addEventListener("resize", updateControlWidth);
+  }, []);
+
+  function updateControlWidth() {
     setControlWidth(window.innerWidth / window.innerHeight * control.current?.offsetHeight);
   }
 
-  function updateControlPosition(position) {
-    if (!isDown) {
-      // Normalizes position
-      let normalizedPosition = -position / (scene.current.offsetWidth - sceneWidth);
-      // Calculates target position and updates
-      setTargetPosition((miniMap.current.offsetWidth - control.current.offsetWidth) * normalizedPosition);
+
+  useImperativeHandle(miniMapRef, () => ({
+    // Moves minimap control control.current when scene is scrolled
+    sceneScrollHandler() {
+      if (!isDownMinimap) {
+        const position = sceneRef.current.scrollLeft;
+        // Normalizes position
+        let normalizedPosition = -position / (sceneRef.current.offsetWidth - sceneWidth);
+        // Calculates target position and updates
+        const targetPosition = (miniMap.current.offsetWidth - control.current.offsetWidth) * normalizedPosition;
+        control.current.style.left = targetPosition + "px";
+      }
     }
-  }
+  }));
+
+
 
   return (
     <div ref={miniMap} className="mini-map small">
-      <img src={thumbnailURL} className="mini-map-thumb" />
-      <div ref={control} className="mini-map-control" style={{ width: controlWidth + "px", left: targetPosition + "px" }}></div>
+      <img src={backgroundURL} className="mini-map-thumb" />
+      <div ref={control} className="mini-map-control" style={{ width: controlWidth + "px" }}></div>
     </div>
   )
 }
--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -2,12 +2,13 @@
 
 import { useEffect, useRef, useState } from 'react';
 import "./scene.css";
-import "./miniMap.css";
+import MiniMap from '../miniMap/MiniMap';
 
 export default function Scene({ backgroundURL, startPosition, children }) {
 
   const sceneRef = useRef(null);
   const backgroundRef = useRef(null);
+  const miniMapRef = useRef();
   const scrollSpeed = 1.2;
 
   let isDownScene = false;
@@ -55,120 +56,6 @@ export default function Scene({ backgroundURL, startPosition, children }) {
   }
 
 
-  // MINI MAP
-
-  let targetPosition;
-  let isDownMinimap = false;
-  let currentPosition;
-  let controlOffset;
-  let minX;
-  let maxX;
-  let padding = 0;
-  let sceneWidth;
-
-  const miniMap = useRef();
-  const control = useRef();
-  const [controlWidth, setControlWidth] = useState();
-
-  // Control handlers
-
-  useEffect(() => {
-
-    sceneWidth = backgroundRef.current?.offsetWidth || 0;
-    controlOffset = control.current.offsetWidth / 2;
-    minX = controlOffset;
-    maxX = miniMap.current.offsetWidth - controlOffset;
-
-    control.current.style.left = 0;
-
-    miniMap.current.addEventListener('mousedown', mouseUp);
-    miniMap.current.addEventListener('touchstart', mouseUp);
-
-    miniMap.current.addEventListener('mouseup', mouseDown);
-    miniMap.current.addEventListener('touchend', mouseDown);
-
-    miniMap.current.addEventListener('mouseup', mouseDown);
-    miniMap.current.addEventListener('touchend', mouseDown);
-
-    miniMap.current.addEventListener('mousemove', mouseMove);
-    miniMap.current.addEventListener('touchmove', mouseMove);
-
-    miniMap.current.addEventListener('mouseenter', () => {
-      isDownMinimap = false;
-      miniMap.current.classList.remove('scrolling');
-    });
-
-    // These two are flipped for some reason. WTF.
-    function mouseUp(e) {
-      isDownMinimap = true;
-      control.current.classList.add('scrolling');
-      startX = e.pageX - miniMap.current.offsetLeft;
-      currentPosition = control.current.style.left;
-    }
-
-    function mouseDown() {
-      isDownMinimap = false;
-      control.current.classList.remove('scrolling');
-    }
-
-    function mouseMove(e) {
-
-      // If mouse is up, do nothing
-      if (!isDownMinimap) return;
-      e.preventDefault();
-      
-      // Gets mouse X relative to the minimap
-      if (e.pageX !== undefined) {
-        targetPosition = e.pageX - miniMap.current.offsetLeft;
-      } else {
-        // Touch screen support
-        targetPosition = e.changedTouches[0].pageX - miniMap.current.offsetLeft;
-      }
-      
-      // Clamps control.current position
-      if (targetPosition < minX + padding) { targetPosition = minX; }
-      else if (targetPosition > maxX - padding) { targetPosition = maxX - padding; }
-      
-      // Offsets for the frame size
-      targetPosition = targetPosition - controlOffset;
-      
-      // Updates the position of the minimap's control.current
-      control.current.style.left = targetPosition + "px";
-      
-      // Normalizes minimap position
-      let normalizedPosition = targetPosition / (miniMap.current.offsetWidth - controlOffset / 2);
-      
-      // Scrolls scene
-      sceneRef.current.scrollLeft = normalizedPosition * sceneWidth;
-
-    }
-
-  });
-
-  
-  useEffect(() => {
-    // Updates minimap control control.current width 
-    updateControlWidth();
-    window.addEventListener("resize", updateControlWidth);
-  }, []);
-
-  function updateControlWidth() {
-    setControlWidth(window.innerWidth / window.innerHeight * control.current?.offsetHeight);
-  }
-
-
-  // Moves minimap control control.current when scene is scrolled
-  function sceneScrollHandler() {
-    if (!isDownMinimap) {
-      const position = sceneRef.current.scrollLeft;
-      // Normalizes position
-      let normalizedPosition = -position / (sceneRef.current.offsetWidth - sceneWidth);
-      // Calculates target position and updates
-      const targetPosition = (miniMap.current.offsetWidth - control.current.offsetWidth) * normalizedPosition;
-      control.current.style.left = targetPosition + "px";
-    }
-  }
-
   return (
     <>
       <div className='scene'
@@ -177,16 +64,17 @@ export default function Scene({ backgroundURL, startPosition, children }) {
         onMouseUp={mouseUpHandler}
         onMouseLeave={mouseLeaveHandler}
         onMouseMove={mouseMoveHandler}
-        onScroll={sceneScrollHandler}
+        onScroll={() => miniMapRef?.current.sceneScrollHandler()}
       >
         <div className='background' ref={backgroundRef} style={{ backgroundImage: `url(${backgroundURL})` }}></div>
         {children}
       </div>
-      {/* Mini map can't be factored out because we useState causes a refresh and stops the scroll event */}
-      <div ref={miniMap} className="mini-map small">
-        <img src={backgroundURL} className="mini-map-thumb" />
-        <div ref={control} className="mini-map-control" style={{ width: controlWidth + "px" }}></div>
-      </div>
+      <MiniMap 
+        sceneRef={sceneRef}
+        backgroundRef={backgroundRef}
+        backgroundURL={backgroundURL}
+        miniMapRef={miniMapRef}
+      />
     </>
   )
 
